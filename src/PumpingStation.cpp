2f16780b298ed0f595e49e8494f5ff7a9df93726
Alexandre Silva <asilva1604@outlook.com>
2024-03-05 15:28:14 +0000

    Added PumpingStation class

---
+++ src/PumpingStation.cpp
@@ -0,0 +1,18 @@
+//
+// Created by alexandre on 05-03-2024.
+//
+
+#include "PumpingStation.h"
+
+PumpingStation::PumpingStation(const unsigned &id, const std::string &code) {
+    id_ = id;
+    code_ = code;
+}
+
+const unsigned int PumpingStation::getId() const {
+    return id_;
+}
+
+const std::string PumpingStation::getCode() const {
+    return code_;
+}
